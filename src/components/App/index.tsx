cd6181f3fcb61c0282d02c8e57bc8f384a85ba66
M.O.K.K.A <mbzmokka@gmail.com>
2023-06-03 13:22:30 +0200

    scroll reset at page change and nav bar opacity

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,74 +1,79 @@
 //Imports
 import React, { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import {
-    selectLanguage,
-    selectSettingsLoaded,
-    selectSettings,
-    localStorageLoad,
-    localStorageLoaded,
+	selectLanguage,
+	selectSettingsLoaded,
+	selectSettings,
+	localStorageLoad,
+	localStorageLoaded,
 } from "../../redux_toolkit";
 import { useDispatch, useSelector } from "react-redux";
 import { ContentModal } from "../content";
 import { SettingsButton, SettingsModal } from "../settings";
 import {
-    HomePage,
-    ErrorNotFoundPage,
-    WorksPage,
-    ContactPage,
+	HomePage,
+	ErrorNotFoundPage,
+	WorksPage,
+	ContactPage,
 } from "../../pages";
 import { StyledBackground } from "./style";
 import { NavBar } from "../navigation";
 
 //Component of the web application
 export default function App() {
-    const { i18n } = useTranslation();
-    const settings = useSelector(selectSettings);
-    const settingsLoaded = useSelector(selectSettingsLoaded);
-    const language = useSelector(selectLanguage);
-    const dispatch = useDispatch();
+	const { i18n } = useTranslation();
+	const settings = useSelector(selectSettings);
+	const settingsLoaded = useSelector(selectSettingsLoaded);
+	const language = useSelector(selectLanguage);
+	const dispatch = useDispatch();
+	const { pathname } = useLocation();
 
-    //applying new language
-    useEffect(() => {
-        i18n.changeLanguage(language);
-        document.documentElement.lang = language;
-    }, [i18n, language]);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
 
-    //loading settings
-    useEffect(() => {
-        if (settingsLoaded === false) {
-            const settingsInStorage = localStorage.getItem("settings");
-            if (settingsInStorage !== null) {
-                dispatch(localStorageLoad(settingsInStorage));
-            }
-            dispatch(localStorageLoaded());
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+	//applying new language
+	useEffect(() => {
+		i18n.changeLanguage(language);
+		document.documentElement.lang = language;
+	}, [i18n, language]);
 
-    //saving settings
-    useEffect(() => {
-        if (settingsLoaded) {
-            let savedSettings = { ...settings };
-            delete savedSettings.loaded;
-            localStorage.setItem("settings", JSON.stringify(savedSettings));
-        }
-    }, [settingsLoaded, settings]);
+	//loading settings
+	useEffect(() => {
+		if (settingsLoaded === false) {
+			const settingsInStorage = localStorage.getItem("settings");
+			if (settingsInStorage !== null) {
+				dispatch(localStorageLoad(settingsInStorage));
+			}
+			dispatch(localStorageLoaded());
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<HomePage />}></Route>
-                <Route path="/work/:id?" element={<WorksPage />}></Route>
-                <Route path="/contact" element={<ContactPage />}></Route>
-                <Route path="*" element={<ErrorNotFoundPage />}></Route>
-            </Routes>
-            <SettingsButton />
-            <NavBar />
-            <SettingsModal />
-            <ContentModal />
-            <StyledBackground />
-        </>
-    );
+	//saving settings
+	useEffect(() => {
+		if (settingsLoaded) {
+			let savedSettings = { ...settings };
+			delete savedSettings.loaded;
+			localStorage.setItem("settings", JSON.stringify(savedSettings));
+		}
+	}, [settingsLoaded, settings]);
+
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<HomePage />}></Route>
+				<Route path="/work/:id?" element={<WorksPage />}></Route>
+				<Route path="/contact" element={<ContactPage />}></Route>
+				<Route path="*" element={<ErrorNotFoundPage />}></Route>
+			</Routes>
+			<SettingsButton />
+			<NavBar />
+			<SettingsModal />
+			<ContentModal />
+			<StyledBackground />
+		</>
+	);
 }
--- src/components/navigation/NavButton/style.tsx
+++ src/components/navigation/NavButton/style.tsx
@@ -5,31 +5,31 @@ import { borders, colors } from "../../../utils/style";
 
 //Types
 type StyledNavButtonProps = {
-    index: number;
-    selected: boolean;
+	index: number;
+	selected: boolean;
 };
 
 //Local styles
 export const StyledNavButton = styled(Link)<StyledNavButtonProps>`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 8px 0px;
-    text-decoration: none;
-    border-right: ${borders.common.blackSlim};
-    border-left: ${({ index }) =>
-        index === 0 ? borders.common.blackSlim : "none"};
-    ${({ selected }) =>
-        selected
-            ? `background-color:${colors.support.navButtonSelected}`
-            : null};
-    img {
-        height: 28px;
-    }
-    p {
-        padding: 8px 0px;
-        font-size: 15px;
-    }
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 8px 0px;
+	text-decoration: none;
+	border-right: ${borders.common.blackSlim};
+	border-left: ${({ index }) =>
+		index === 0 ? borders.common.blackSlim : "none"};
+	${({ selected }) =>
+		selected ? `background-color:${colors.support.navButtonSelected}` : null};
+	img {
+		height: 28px;
+		opacity: ${({ selected }) => (selected ? 1 : 0.5)};
+	}
+	p {
+		padding: 8px 0px;
+		font-size: 15px;
+		opacity: ${({ selected }) => (selected ? 1 : 0.5)};
+	}
 `;
